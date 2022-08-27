51ad760c511b242b682f4094f9de09ff540e2650
Nigel Barnes <barnesnigel52@gmail.com>
2022-08-27 17:53:18 -0400

    Changed a little more

--- src/main/java/com/revature/controllers/ORMcontroller.java
+++ src/main/java/com/revature/controllers/ORMcontroller.java
@@ -4,17 +4,13 @@ import java.util.List;
 
 import com.revature.models.ORMModel;
 import com.revature.services.ORMService;
-import com.revature.models.*;
 
 public class ORMcontroller {
 	
 	ORMModel modelORM = new ORMModel();
 	ORMService ormService = new ORMService();
 	
-	String databaseURL = "jdbc:postgresql://javafs220725.c7uqwmtdtwsg.us-east-1.rds.amazonaws.com:5432/project1";
-	String databaseUN = "postgres";
-	String databasePW = "password";
-	String databaseDriverName = "org.postgresql.Driver";
+	
 	
 	public List<Object> retrieveAllObjectsController(Object objectFromWeb,  String databaseURL, String databaseUN, String databasePW, String databaseDriverName){
 		modelORM.setConnectionURL(databaseURL);
